dbce656879e66f432ff73e2f111757276d342fd8
Giorgio Cantoni <giorgio.canto98@gmail.com>
2024-08-31 11:18:20 +0200

    updated bottomsheet logic and favicons

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,17 @@ document.getElementById('avatar').addEventListener('click', function() {
 });
 
 document.getElementById('closeBottomSheet').addEventListener('click', function() {
+    closeBottomSheet();
+});
+
+document.addEventListener('click', function(event) {
+    if (!document.getElementById('bottomSheet').contains(event.target) && !document.getElementById('avatar').contains(event.target)) {
+        closeBottomSheet();
+    }
+});
+
+function closeBottomSheet() {
     document.getElementById('bottomSheet').style.bottom = '-100%';
     document.querySelector('.fake-div').classList.remove('blur-background');
     document.querySelector('.fake-div2').classList.remove('blur-background');
-});
+}
