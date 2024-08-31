6cf9bf288dff45ad4d7ea0f2a135ba89996704f2
Giorgio Cantoni <giorgio.canto98@gmail.com>
2024-08-31 17:04:44 +0200

    imp

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,19 @@
-document.getElementById('avatar').addEventListener('click', function() {
-    document.getElementById('bottomSheet').style.bottom = '30px';
+function openBottomSheet() {
+    const bottomSheet = document.getElementById('bottomSheet');
+    bottomSheet.style.bottom = '30px';
     document.querySelector('.fake-div').classList.add('blur-background');
     document.querySelector('.fake-div2').classList.add('blur-background');
+}
+
+function closeBottomSheet() {
+    const bottomSheet = document.getElementById('bottomSheet');
+    bottomSheet.style.bottom = '-100%';
+    document.querySelector('.fake-div').classList.remove('blur-background');
+    document.querySelector('.fake-div2').classList.remove('blur-background');
+}
+
+document.getElementById('avatar').addEventListener('click', function() {
+    openBottomSheet();
 });
 
 document.getElementById('closeBottomSheet').addEventListener('click', function() {
@@ -9,13 +21,10 @@ document.getElementById('closeBottomSheet').addEventListener('click', function()
 });
 
 document.addEventListener('click', function(event) {
-    if (!document.getElementById('bottomSheet').contains(event.target) && !document.getElementById('avatar').contains(event.target)) {
+    const bottomSheet = document.getElementById('bottomSheet');
+    const avatar = document.getElementById('avatar');
+
+    if (!bottomSheet.contains(event.target) && !avatar.contains(event.target)) {
         closeBottomSheet();
     }
 });
-
-function closeBottomSheet() {
-    document.getElementById('bottomSheet').style.bottom = '-100%';
-    document.querySelector('.fake-div').classList.remove('blur-background');
-    document.querySelector('.fake-div2').classList.remove('blur-background');
-}
